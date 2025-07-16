7a918f9a533e84cf3dd9017fb83b27b9b739b297
Badar Rehman <badar.rehman137@gmail.com>
2025-07-17 01:39:03 +0500

    Initial Commit

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,11 +4,6 @@ public class GameManager : MonoBehaviour
 {
     private void Awake()
     {
-        // Ensure there's an EventSystem in the scene
-        if (FindObjectOfType<UnityEngine.EventSystems.EventSystem>() == null)
-        {
-            var eventSystem = new GameObject("EventSystem", typeof(UnityEngine.EventSystems.EventSystem), 
-                typeof(UnityEngine.EventSystems.StandaloneInputModule));
-        }
+        
     }
 }
